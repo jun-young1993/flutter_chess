47329768575759f5565ce137046ba960d425ab4d
kim-jun-young <jykim@gemiso.com>
2024-08-22 20:50:18 +0900

    init

--- lib/ui/screen/standard/standard_chess_board_screen.dart
+++ lib/ui/screen/standard/standard_chess_board_screen.dart
@@ -13,6 +13,7 @@ class StandardChessBoardScreen extends StatefulWidget{
 
 class _StandardChessBoardScreenState extends State<StandardChessBoardScreen> {
   final StandardChessBoard _board = StandardChessBoard();
+  
 
   @override
   Widget build(BuildContext context){
@@ -20,13 +21,28 @@ class _StandardChessBoardScreenState extends State<StandardChessBoardScreen> {
       body: ChessGrid(
         crossAxisCount: 8,
         itemBuilder: (context, index, row, col){
+          ChessPiece? piece = _board.getPiece(row, col);
+
           return BoardTile(
-            piece: _board.getPiece(row, col),
+            piece: piece,
             index: index, 
             row: row,
             col: col,
+            isSelected: _board.isSameAsPosition(row,col),
+            isPossibleMove: _board.isPossiblePosition(row,col),
             onTap: (){
-              print('on tab');
+              setState((){
+                if(_board.isSelected()){
+                  if(_board.isPossiblePosition(row,col)){
+                    _board.move(Position(row, col));
+                  }else{
+                    _board.unSelect();
+                  }
+                }else{
+                  _board.select(row, col);
+                }
+              });
+            
             }, 
             
           );
